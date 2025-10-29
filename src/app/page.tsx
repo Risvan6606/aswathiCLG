4a099438db083d4fd3dc68b228d4cadb5b8651c0
Risvan6606 <risvangalfan@gmail.com>
2025-10-29 14:41:52 +0400

    name changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Testimonial from "@/components/Home/Testimonials";
 import Newsletter from "@/components/Home/Newsletter";
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "adish",
+  title: "eLearning",
 };
 
 export default function Home() {
